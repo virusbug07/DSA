888a6c01d476f1b6ad4151712c53a76df267328d
Ashutosh Dubey <77336701+virusbug07@users.noreply.github.com>
2023-05-05 18:36:26 +0530

    Create move all zeroes to end of array.java

---
+++ move all zeroes to end of array.java
@@ -0,0 +1,14 @@
+import java.io.*;
+ 
+class PushZero
+{  
+    static void pushZerosToEnd(int arr[], int n)
+    {
+        int count = 0;   
+        for (int i = 0; i < n; i++)
+            if (arr[i] != 0)
+                arr[count++] = arr[i]; /
+        while (count < n)
+            arr[count++] = 0;
+    }
+ 
