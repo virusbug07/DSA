3e9e1ce269186d81622d91723f7de2ffab28a236
Ashutosh Dubey <77336701+virusbug07@users.noreply.github.com>
2023-05-09 19:51:20 +0530

    Create Search, insert and delete in a sorted array.java

---
+++ Search, insert and delete in a sorted array.java
@@ -0,0 +1,17 @@
+class Main {
+  
+    static int binarySearch(int arr[], int low, int high,
+                            int key)
+    {
+        if (high < low)
+            return -1;
+ 
+        /*low + (high - low)/2;*/
+        int mid = (low + high) / 2;
+        if (key == arr[mid])
+            return mid;
+        if (key > arr[mid])
+            return binarySearch(arr, (mid + 1), high, key);
+        return binarySearch(arr, low, (mid - 1), key);
+    }
+ 
