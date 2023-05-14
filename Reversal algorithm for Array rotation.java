dfd5192cf45d6c7d7a251e066b704306059f9d98
Ashutosh Dubey <77336701+virusbug07@users.noreply.github.com>
2023-05-14 22:45:00 +0530

    Create Reversal algorithm for Array rotation.java

---
+++ Reversal algorithm for Array rotation.java
@@ -0,0 +1,34 @@
+import java.io.*;
+ 
+class LeftRotate {
+    static void leftRotate(int arr[], int d)
+    {
+ 
+        if (d == 0)
+            return;
+ 
+        int n = arr.length;
+        d = d % n;
+        reverseArray(arr, 0, d - 1);
+        reverseArray(arr, d, n - 1);
+        reverseArray(arr, 0, n - 1);
+    }
+ 
+    static void reverseArray(int arr[], int start, int end)
+    {
+        int temp;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+ 
+  
+    static void printArray(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
