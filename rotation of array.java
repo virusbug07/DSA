73c4e002f380690b37b225ac46516d87896e2895
Ashutosh Dubey <77336701+virusbug07@users.noreply.github.com>
2023-05-15 21:55:57 +0530

    Create rotation of array.java

---
+++ rotation of array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+ 
+public class Test
+{
+    static int arr[] = new int[]{1, 2, 3, 4, 5};
+     
+    // Method for rotation
+    static void rotate()
+    {
+       int x = arr[arr.length-1], i;
+       for (i = arr.length-1; i > 0; i--)
+          arr[i] = arr[i-1];
+       arr[0] = x;
+    }
