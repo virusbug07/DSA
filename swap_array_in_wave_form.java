6ec23c155657b66dd03c0b19957c6c6185a03a94
Ashutosh Dubey <77336701+virusbug07@users.noreply.github.com>
2023-05-23 13:14:57 +0530

    Create swap_array_in_wave_form.java

---
+++ swap_array_in_wave_form.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+class SortWave
+{
+    void swap(int arr[], int a, int b)
+    {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    void sortInWave(int arr[], int n)
+    {
+        Arrays.sort(arr);
+        for (int i=0; i<n-1; i += 2)
+            swap(arr, i, i+1);
+    }
+ 
